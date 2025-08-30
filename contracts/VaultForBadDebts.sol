d3a7955bc59ddd835acdf779690e54728624529e
innominatissimus <innominatissimus@proton.me>
2025-08-30 19:59:11 +0300

    Make vault ownable

--- contracts/VaultForBadDebts.sol
+++ contracts/VaultForBadDebts.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.29;
 
 import "@openzeppelin-contracts-5.4.0/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin-contracts-5.4.0/token/ERC20/IERC20.sol";
+import "@openzeppelin-contracts-5.4.0/access/Ownable.sol";
 
-contract VaultForBadDebts is ERC4626
+contract VaultForBadDebts is ERC4626, Ownable
 {
-	constructor(IERC20 _asset) ERC4626(_asset) ERC20("Bad Debt Workout Vault", "BDWV")
+	constructor(IERC20 _asset) ERC4626(_asset) ERC20("Bad Debt Workout Vault", "BDWV") Ownable(msg.sender)
 	{
 		
 	}
